4d24487b3347349e908503654221db5e25768a01
Michael Garten <mjgartendev@gmail.com>
2019-05-31 10:25:54 -0500

    separate markup, style, and script editors

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,22 @@
 <script>
   import * as monaco from 'monaco-editor';
   import {onMount} from 'svelte';
-  import {generateApp} from './compileSource';
 
   $: theme = monaco.editor.setTheme(theme);
   let previewType;
-  let value;
-  let svelteSource = {};
+  $: value = `${svelteSource.html}<style>${svelteSource.css}</style><script>${svelteSource.js}<\/script>`;
+  let svelteSource = {
+    html: '',
+    css: '',
+    js: '',
+  };
   export let name = "SvelteMonaco Sandbox";
   export let user = "Stranger";
   function updatePreview(){
-    value = monaco.editor.getModel('inmemory://model/1').getValue();
-    generateApp().then(function(response) {
-      svelteSource += response.json()
-    });
+    svelteSource.html = monaco.editor.getModel('inmemory://model/1').getValue();
+    svelteSource. css = monaco.editor.getModel('inmemory://model/2').getValue();
+    svelteSource.js = monaco.editor.getModel('inmemory://model/3').getValue();
+    console.log(svelteSource)
   }
   onMount(() => updatePreview())
   </script>
@@ -27,8 +30,9 @@
   grid-template-areas: "top top top" "nav input output" "nav btm btm";
   color: white;
   font-family: Roboto;
-	height: 100vh;
-	width: 100vw;
+	height: 100%;
+  width: 100vw;
+  box-sizing: border-box;
 }
 #code{grid-area: input}
 #preview {
@@ -158,16 +162,21 @@ a.nav-item:hover {
   </aside>
   <section id="code"  class="app-panel">
     <header class="panel-header">
-      <button class="btn"><span class="mdi mdi-36 mdi-pencil"></span></button>
-      <p class="title">markup</p>
-      <select bind:value={theme}>
-          <option>vs-dark</option>
-          <option>vs</option>
-        </select>
+        <button class="btn"><span class="mdi mdi-36 mdi-pencil"></span></button>
+        <p class="title">Svelte Component</p>
+        <select bind:value={theme}>
+            <option>vs-dark</option>
+            <option>vs</option>
+          </select>
     </header>
-    <div class="panel-content">
-      <div on:keydown={updatePreview} id="input" style="height: 600px; width: 800px;"></div> 
-    </div>
+    <div class="panel-content" style="display: flex; flex-direction: column;">
+      <span>Markup</span>
+      <div on:keydown={updatePreview} id="markup" style="height: 100%; width: 100%;"></div>
+      <span>Style</span>
+      <div on:keydown={updatePreview} id="style" style="height: 100%; width: 100%;"></div> 
+      <span>Script</span>
+      <div on:keydown={updatePreview} id="script" style="height: 100%; width: 100%;"></div> 
+   </div>
   </section>
   <section id="preview" class="app-panel">
     <header class="panel-header">
@@ -204,14 +213,24 @@ a.nav-item:hover {
 
 <svelte:head>
 <script>  
-monaco.editor.create(document.getElementById('input'), {
-	value: [
-		'<script>\n\tlet name = "SvelteMonaco";\n<\/script>\n',
-		'<style>\n\th1 {\n\t\tcolor: mediumseagreen\n\t}\n</style>\n',
-    '\n<h1>Hello {name}!</h1>',
-    'name: <input bind:value={name}>'
-	].join('\n'),
-	language: 'html'
-});
+  monaco.editor.create(document.getElementById('markup'), {
+    value: [
+      '<h1>Hello {name}!</h1>',
+      'name: <input bind:value={name}>'
+    ].join('\n'),
+    language: 'html'
+  });
+  monaco.editor.create(document.getElementById('style'), {
+    value: [
+      'h1 {\n\tcolor: mediumseagreen\n}\n',
+    ].join('\n'),
+    language: 'css'
+  });
+  monaco.editor.create(document.getElementById('script'), {
+    value: [
+      'let name = "SvelteMonaco";',
+    ].join('\n'),
+    language: 'javascript'
+  });
   </script>
 </svelte:head>
